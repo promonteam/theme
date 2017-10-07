8018e8e81bef96c82e7e21cd21b34e226fd058a1
Ervin <glugox@gmail.com>
2017-10-07 13:51:33 +0200

    Fixed menu float

--- wp-content/themes/promon/index.php
+++ wp-content/themes/promon/index.php
@@ -17,13 +17,13 @@ get_header(); ?>
     <section id="fh5co-header">
         <div class="container">
             <nav role="navigation">
-                <ul class="pull-left left-menu">
+                <ul class="float-md-left left-menu">
                     <li><a href="about.html">About</a></li>
                     <li><a href="tour.html">Tour</a></li>
                     <li><a href="pricing.html">Pricing</a></li>
                 </ul>
                 <h1 id="fh5co-logo"><a href="index.html">guide<span>.</span></a></h1>
-                <ul class="pull-right right-menu">
+                <ul class="float-md-right right-menu">
                     <li><a href="#">Login</a></li>
                     <li class="fh5co-cta-btn"><a href="#">Sign up</a></li>
                 </ul>
