d6e46cf902d57aa516ad47b9174cb43323ccc41d
Adnan <adnan.cutura@gmail.com>
2017-10-07 12:35:08 +0200

    Dinamic loaded CSS

---
+++ wp-content/themes/promon/functions.php
@@ -0,0 +1,12 @@
+<?php
+function add_theme_scripts() {
+    wp_enqueue_style( 'animate.css', get_template_directory_uri() . '/css/animate.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'flexslider.css', get_template_directory_uri() . '/css/flexslider.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'icomoon.css', get_template_directory_uri() . '/css/icomoon.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'magnific-popup.css', get_template_directory_uri() . '/css/magnific-popup.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'bootstrap.min.css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'style.css', get_template_directory_uri() . 'style.css', array(), '1.1', 'all');
+}
+
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+ ?>
